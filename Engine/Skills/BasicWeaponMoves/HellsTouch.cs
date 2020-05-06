99a5f99f053500c56683a588290e399b5d3c9291
JMatt7 <44380357+JMatt7@users.noreply.github.com>
2020-05-07 00:09:05 +0200

    added new some skill and decorators for them

---
+++ Engine/Skills/BasicWeaponMoves/HellsTouch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Engine.CharacterClasses;
+
+namespace Game.Engine.Skills.BasicWeaponMoves
+{
+    class HellsTouch : Skill
+    {
+        public HellsTouch() : base("Hell's Touch", 25, 2)
+        {
+            PublicName = "Hell's Touch Attack [requires axe]: ";
+            RequiredItem = "Axe";
+        }
+
+        public override List<StatPackage> BattleMove(Player player)
+        {
+            StatPackage resp = new StatPackage("incised");
+            resp.HealthDmg = (int)(0.3 * player.Strength) + (int)(0.2 * player.Level);
+            resp.CustomText = "You use Hell's Touch Attack! (" + (int)(0.3 * player.Strength) + (int)(0.2 * player.Level) + " incised damage!)";
+            return new List<StatPackage>() { resp };
+        }
+    }
+}
